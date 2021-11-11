b823e0d2c1e379c4a16b3dd91bf6239d06b15c5d
Przemyslaw.Kozak.QA <przemyslaw.kozak.qa@gmail.com>
2021-11-11 10:47:43 +0100

    projecyt structure setup

---
+++ src/main/java/utils/BaseUrlUtils.java
@@ -0,0 +1,5 @@
+package utils;
+
+public class BaseUrlUtils {
+    public static final String BASE_URL = "http://automationpractice.com/index.php";
+}
---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utils.BaseUrlUtils;
+import utils.PageTitleUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setupTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(BaseUrlUtils.BASE_URL);
+        assertThat(driver.getTitle()).isEqualTo(PageTitleUtils.HOME_PAGE_TITLE);
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
